60fcda523887f998470ee8417bb9f21111c05fef
SathwikUK <sathwiksampengala@gmail.com>
2025-07-18 18:30:05 +0530

    url chnage

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://invigo-sathwikuks-projects.vercel.app",
+  baseURL: "https://invigo23-sathwikuks-projects.vercel.app/",
 });
 
